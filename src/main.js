932b2987c94c6792f09cd5dfbb24caba5da49657
ekowebdev <ekoprasetyo.webdev@gmail.com>
2023-07-19 16:21:44 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import {web} from "./application/web.js";
 import {logger} from "./application/logging.js";
 
-web.listen(3000, () => {
+web.listen(process.env.APP_PORT, () => {
     logger.info(`App Start || Port: ${process.env.APP_PORT}`);
 });
